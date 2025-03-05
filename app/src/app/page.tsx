b3f43fde0fd2abdbacec6e2d55eaa8bb80f58ed1
Rishi-Prasanna <rishi.b.prasanna@gmail.com>
2025-03-05 18:53:53 +0530

    Implemented Navbar Somewhat
    Need to import fonts and font-awesome next.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Navbar from "../components/navbar";
 
 export default function Home() {
   return (
+    <>
+    <Navbar links={[
+      {title: "Home", href: "#"},
+      {title: "About", href: "#"},
+      {title: "Board Editor", href: "#"},
+      {title: "Openings", href: "#"},
+      {title: "Contact", href: "#"},
+    ]}/>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -97,5 +106,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </>
   );
 }
